b852d7ce2c343d539e18052fe91f59abc0ccc9d0
thousifsyed04 <thousifsyed28@gmail.com>
2026-04-06 02:00:20 +0530

    UC6

--- src/com/bridgelabz/HelloApp.java
+++ src/com/bridgelabz/HelloApp.java
@@ -5,8 +5,13 @@ import java.util.Scanner;
 public class HelloApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
-        System.out.println("Hello " + name);
+        System.out.println("Enter number of names: ");
+        int count = scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 0; i < count; i++) {
+            System.out.println("Enter name: ");
+            String name = scanner.nextLine();
+            System.out.println("Hello " + name);
+        }
     }
 }
