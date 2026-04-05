983d30f890134d48449a0ffed070748e7adef740
thousifsyed04 <thousifsyed28@gmail.com>
2026-04-06 01:53:59 +0530

    UC3

--- src/com/bridgelabz/HelloApp.java
+++ src/com/bridgelabz/HelloApp.java
@@ -2,6 +2,10 @@ package com.bridgelabz;
 
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello " + args[0]);
+        if (args.length > 0) {
+            System.out.println("Hello " + args[0]);
+        } else {
+            System.out.println("Hello World");
+        }
     }
 }
